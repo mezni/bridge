3f8ab41c3ce63fef482605a2e80611a81736dcad
mezni <mamezni@gmail.com>
2024-11-24 22:45:49 +0000

    gencdr 2024-11-24

--- tools/cdrgen/cmd/main.go
+++ tools/cdrgen/cmd/main.go
@@ -8,19 +8,9 @@ import (
 
 
 func main() {
-	// Create a map to hold context key-value pairs
 	contextData := make(map[any]any)
-	contextData[logger.CustomContextKey("requestID")] = "12345"
-	contextData[logger.CustomContextKey("userID")] = 789
-
-	// Initialize the context with multiple key-value pairs
-	ctx := context.WithValue(context.Background(), "context", contextData)
-
-	// Example usage of the logger with context
-	loggerWithContext := logger.NewSlogLogger(logger.InfoLevel, "HTTPServer", ctx)
-	loggerWithContext.Debug("This is a debug message", "key1", "debug_value") // Debug level logs
-
-	// Example usage of the logger without context
-	loggerWithoutContext := logger.NewSlogLogger(logger.InfoLevel, "HTTPServer")
-	loggerWithoutContext.Info("This is an info message", "key1", "value1") // Info level logs
+ctx := context.WithValue(context.Background(), "context", contextData)
+	logger := logger.NewSlogLogger(logger.InfoLevel, "myModule",ctx)
+	logger.Info("This is an info message", "key1", "value1") 
+	logger.Debug("This is an info message", "key1", "value1") 
 }
