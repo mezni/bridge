d2c4fcad8427857d5acf27b2e0214f6e70777e70
mezni <mamezni@gmail.com>
2024-12-15 20:09:54 +0000

    gencdr 2024-12-15

--- reports/test.py
+++ reports/test.py
@@ -1,6 +1,12 @@
 def print_mesure(name, mesure):
     print(f"{name} = {mesure}\n")
 
+chars = { "down_char": "UNICHAR(9660)",
+    "steady_char" : "UNICHAR(9679)",
+    "up_char" : "UNICHAR(9650)"}
+
+
+
 def generate_mesure(config):
     name = "test"
     table_name = config.get("table_name")
@@ -11,7 +17,11 @@ def generate_mesure(config):
     
     variables_section = "\n"
     variables_section += "\n".join(f"  {line}" for line in strings_list)
-    
+
+    for key, value in chars.items():
+        print(f"\t{key}: {value}")
+        variables_section += f"\n  VAR {key} = {value}"
+
     # Create the mesure string
     mesure = f"""{variables_section}"""
     return name, mesure 
